2fd23e809f6c099ab3a3467fb1b6d86f1a44826c
ratalay <rahmiiatalay@gmail.com>
2021-01-24 21:44:05 +0000

    checkUp function REPL tested, unit test to be written

--- pet.js
+++ pet.js
@@ -26,6 +26,18 @@ Pet.prototype.feed = function () {
     }
 }
 
+Pet.prototype.checkUp = function () {
+if (this.hunger >= 5 && this.fitness <= 3) {
+    return 'I am hungry AND I need a walk'
+} else if (this.hunger < 5 && this.fitness > 3){
+    return 'I feel great!'
+} else if (this.hunger >= 5) {
+    return 'I am hungry'
+} else {
+    return 'I need a walk'
+}
+}
+
 
  
  
